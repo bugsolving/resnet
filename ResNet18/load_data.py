51625238defc241d7fcd0baa61b9b6118f8d76e6
Su Jun <u20js22@abdn.ac.uk>
2024-03-12 22:51:38 +0800

    Update load_data.py

--- ResNet18/load_data.py
+++ ResNet18/load_data.py
@@ -1,51 +1,3 @@
-# import torch
-# import pandas as pd
-# from preprocess import get_files, get_dirs
-# import numpy as np
-
-
-# # def load(img_path, label_path):
-# #     label = pd.read_csv(label_path)
-# #     types = ['Freeform', 'Northwind']
-# #     paths, labels = [], []
-# #     for t in types:
-# #         dirs = get_dirs(img_path + t)
-# #         for d in dirs:
-# #             no = d[-5:]
-# #             l = label[label['file'] == no]['label'].to_numpy()[0]
-# #             files = get_files(d)
-# #             for file in files:
-# #                 paths.append(d + '/' + file)
-# #                 labels.append(l)
-# #     return paths, torch.from_numpy(np.array(labels)).view((len(labels), 1))
-
-
-# def load(img_path, label_path):
-#    label = pd.read_csv(label_path)
-#    types = ['Freeform', 'Northwind']
-#    paths, labels = [], []
-#    for t in types:
-#       dirs = get_dirs(img_path + t)
-#       for d in dirs:
-#          files = get_files(d)
-#          for file in files:
-#             no = file
-#             l = label[label['file'] == no]['label'].to_numpy()[0]
-#             paths.append(d + '/' + file)
-#             labels.append(l)
-#    return paths, torch.from_numpy(np.array(labels)).view((len(labels), 1))
-
-
-
-# if __name__ == "__main__":
-#    img_path = "your_img_path"
-#    label_path = "G:\keep\label.csv"
-#    paths, labels = load(img_path, label_path)
-#    print(paths)
-#    print(labels)
-
-
-
 import torch
 import pandas as pd
 from preprocess import get_files, get_dirs
@@ -74,8 +26,8 @@ def load(img_path, label_path):
     return paths, torch.from_numpy(np.array(labels)).view((len(labels), 1))
 
 if __name__ == "__main__":
-    img_path = "G:\\keep\\test"  # Update this path with the correct one.
-    label_path = "G:\\keep\\label.csv"  # Ensure this path is correct and accessible.
+    img_path = "./test"  # Update this path with the correct one.
+    label_path = "./label.csv"  # Ensure this path is correct and accessible.
     paths, labels = load(img_path, label_path)
     print("Final paths:")
     print(paths)
